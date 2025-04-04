914749f7ca7dfc28ca8b3457a011b473aa918c33
wakana <wakana.toshiyuki@marble-corp.co.jp>
2025-04-04 13:49:59 +0900

    update directory structure

--- FCBTool/common/CreateFile.h
+++ FCBTool/common/CreateFile.h
@@ -1,1 +1,10 @@
+﻿/*!
+ * @file  CreateFile.h
+ * @brief CreateFile 用マクロ定義
+ * @date 2025
+ */
+#pragma once
 
+#define LONG_PRE_PATH	L"\\\\?\\"
+#define LONG_PRE_PATH_LEN	(4)
+#define LONG_MAX_PATH	(32767)
